327760ccf06087610a3707e0a6c1cacf5bd27c52
Pedram Ghane <p.ghane@gmail.com>
2026-03-13 11:58:45 +0100

    refactor: Blog Single Layout Design

--- single.php
+++ single.php
@@ -1,17 +1,17 @@
 <?php get_header(); ?>
-<div class="component blog" style="margin-bottom:8rem;">
-  <div class="container px-4 sm:px-0">
-    <div class="blog-header flex flex-col-reverse sm:flex-row gap-10 sm:gap-20">
-      <div class="relative flex items-center basis-full sm:basis-1/2">
-        <h1 class="mb-4 fs-4 lh-lg" style="font-size: unset;!important"><?php the_title(); ?></h1>
-        <!-- <p class="w-4/5"><?php the_excerpt(); ?></p> -->
-        <div class="absolute bottom-[75px] right-[50px]"><img src="<?php echo get_template_directory_uri() ?>/assets//images/sign-2-v-gray.svg" alt="" /></div>
-      </div>
-      <div class="basis-full sm:basis-1/2">
-        <?php if (has_post_thumbnail(get_the_id())) echo get_the_post_thumbnail(get_the_id(), 'full', array('class' => 'single-thumb img-fluid w-100 rounded-4')); ?>
+<div class="_component single-blog">
+  <div class="max-w-4xl mx-auto px-4">
+    <div class="blog-header flex flex-col-reverse sm:flex-row items-center gap-4 sm:gap-8">
+      <?php if (has_post_thumbnail(get_the_id())) echo get_the_post_thumbnail(get_the_id(), 'full', array('class' => 'rounded-2xl aspect-square sm:w-64 object-cover object-center')); ?>
+
+      <div class="flex h-full">
+        <h1 class="text-3xl sm:text-4xl font-bold uppercase text-primary sm:w-3/4"><?php the_title(); ?></h1>
+
+        <div class="hidden sm:flex justify-end flex-1"><img src="<?php echo get_template_directory_uri() ?>/assets/images/sign-2-v-gray.svg" alt="ReoNet Symbol Pattern" /></div>
       </div>
     </div>
-    <div class="blog-body mt-5 sm:mt-10">
+
+    <div class="blog-body my-8 sm:mt-12 sm:mb-20 sm:bg-gray-100/60 p-0 sm:p-10">
       <?php the_content(); ?>
     </div>
   </div>
